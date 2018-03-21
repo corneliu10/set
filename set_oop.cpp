75b7315f6ae27229f2341a03c09bba4c90e2b45a
Corneliu Dumitru <corneliu.dcv@gmail.com>
2018-03-21 22:52:46 +0200

    add destr and some operators

--- set_oop.cpp
+++ set_oop.cpp
@@ -1,230 +1,438 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <vector>
 
 using namespace std;
 
 class Multime
 {
-    public:
-        Multime(){
-            card = 0;
-        }
+public:
+    Multime(){
+        card = 0;
+    }
+    
+    Multime(const Multime &A) {
+        card = A.card;
         
-        void vect_mult()
-        {
-            if(card==0) return;
-            
-            int *X,cardX = 0;
-            sort(M,M+card);
-            
-            cardX++; // elem 1
-            for(int i=1;i<card;i++)
-                if(M[i] != M[i-1]) cardX++;
-                
-            X = (int*)malloc(sizeof(int) * cardX);
-            X[0] = M[0];cardX = 1;
-            
-            for(int i=1;i<card;i++)
-                if(M[i] != M[i-1]) X[cardX++] = M[i];
-                
-            free(M);
-            M = X;
-            card = cardX;
-        }
+        M = new int [card];
         
-        friend istream &operator>> (istream &in, Multime &D)
-        {
-            //cout<<"Cardinalul:";
-            in>>D.card;
-            D.M = (int*)malloc(sizeof(int) * D.card);
-            
-            for(int i = 0;i < D.card; ++i)
-                in>>D.M[i];
-            
-            D.vect_mult();
-            
-            return in;
-        }
+        for(int i = 0;i < card; ++i)
+            M[i] = A.M[i];
+    }
+    
+    ~Multime() {
+        if(card==1)
+            delete M;
+        else
+            delete[] M;
+    }
+    
+    void vect_mult()
+    {
+        if(card==0) return;
         
-        friend ostream &operator<< (ostream &out, Multime &D)
+        int *X,cardX = 0;
+        sort(M,M+card);
+        
+        cardX++; // elem 1
+        for(int i=1;i<card;i++)
+            if(M[i] != M[i-1]) cardX++;
+        
+        X = new int [cardX]; //(int*)malloc(sizeof(int) * cardX);
+        X[0] = M[0];cardX = 1;
+        
+        for(int i=1;i<card;i++)
+            if(M[i] != M[i-1]) X[cardX++] = M[i];
+        
+        M = X;
+        card = cardX;
+    }
+    
+    friend istream &operator>> (istream &in, Multime &D)
+    {
+        //cout<<"Cardinalul:";
+        in>>D.card;
+        D.M = new int [D.card]; //(int*)malloc(sizeof(int) * D.card);
+        
+        for(int i = 0;i < D.card; ++i)
+            in>>D.M[i];
+        
+        D.vect_mult();
+        
+        return in;
+    }
+    
+    friend ostream &operator<< (ostream &out, Multime &D)
+    {
+        out<<D.card<<"\n{";
+        
+        if(D.card == 0)
         {
-            out<<D.card<<"\n";
-            
-            for(int i = 0;i < D.card; ++i)
-                out<<D.M[i]<<" ";
-            
-            out<<"\n";
+            out<<"{}\n";
+        
             return out;
         }
         
-        Multime operator+(Multime& A) {
-            Multime B;
-            int i;
-            
-            B.card = this->card + A.card;
-            
-            B.M = (int*)malloc(sizeof(int) * B.card);
-            for(i=0;i < this->card;++i)
-                B.M[i] = this->M[i];
-            
-            for(;i < B.card;++i)
-                B.M[i] = A.M[i - this->card];
-            
-            B.vect_mult();
-            return B;
-        }
+        for(int i = 0;i < D.card - 1; ++i)
+            out<<D.M[i]<<",";
         
-        Multime operator*(Multime& A) {
-            Multime B,C;
-            int i;
-            
-            B.card = this->card + A.card;
-            
-            B.M = (int*)malloc(sizeof(int) * B.card);
-            for(i=0;i < this->card;++i)
-                B.M[i] = this->M[i];
-            
-            for(;i < B.card;++i)
-                B.M[i] = A.M[i - this->card];
-            
-            sort(B.M,B.M + B.card);
-            
-            for(i=1;i<B.card;++i)
-                if(B.M[i] == B.M[i-1]) C.card++;
-            
-            C.M = (int*)malloc(sizeof(int) * C.card);
-            C.card = 0;
-            
-            for(i=1;i<B.card;++i)
-                if(B.M[i] == B.M[i-1]) C.M[card++] = B.M[i];
-            
-            return C;
-        }
-
-        Multime operator-(Multime &A) {
-        	map<int, bool> T;
-        	Multime B;
-        	int i;
-
-        	for(i=0;i<A.card;i++)
-        		T.insert(make_pair(A.M[i],true));
-
-        	for(i=0;i<this->card;++i)
-        		if(T.find(this->M[i]) == T.end())
-        			B.card++;
-
-        	B.M = (int*)malloc(sizeof(int) * B.card);
-        	B.card = 0;
-
-        	for(i=0;i<this->card;++i)
-        		 if(T.find(this->M[i]) == T.end())
-        			B.M[B.card++] = this->M[i];
-
-        	return B;
-        }
+        out<<D.M[D.card - 1];
         
-    private:
-        int *M;
-        int card;
+        out<<"}\n";
+        return out;
+    }
+    
+    Multime operator+(Multime& A) {
+        Multime B;
+        int i;
+        
+        B.card = this->card + A.card;
+        
+        B.M = new int [B.card]; //(int*)malloc(sizeof(int) * B.card);
+        for(i=0;i < this->card;++i)
+            B.M[i] = this->M[i];
+        
+        for(;i < B.card;++i)
+            B.M[i] = A.M[i - this->card];
+        
+        B.vect_mult();
+        return B;
+    }
+    
+    Multime operator*(Multime& A) {
+        Multime B,C;
+        int i;
+        
+        B.card = this->card + A.card;
+        
+        B.M = new int [B.card]; //(int*)malloc(sizeof(int) * B.card);
+        for(i=0;i < this->card;++i)
+            B.M[i] = this->M[i];
+        
+        for(;i < B.card;++i)
+            B.M[i] = A.M[i - this->card];
+        
+        sort(B.M,B.M + B.card);
+        
+        for(i=1;i<B.card;++i)
+            if(B.M[i] == B.M[i-1]) C.card++;
+        
+        C.M = new int [C.card]; //(int*)malloc(sizeof(int) * C.card);
+        C.card = 0;
+        
+        for(i=1;i<B.card;++i)
+            if(B.M[i] == B.M[i-1]) C.M[C.card++] = B.M[i];
+        
+        return C;
+    }
+    
+    Multime operator-(Multime &A) {
+        map<int, bool> T;
+        Multime B;
+        int i;
+        
+        for(i=0;i<A.card;i++)
+            T.insert(make_pair(A.M[i],true));
+        
+        for(i=0;i<this->card;++i)
+            if(T.find(this->M[i]) == T.end())
+                B.card++;
+        
+        B.M = new int [B.card]; //(int*)malloc(sizeof(int) * B.card);
+        B.card = 0;
+        
+        for(i=0;i<this->card;++i)
+            if(T.find(this->M[i]) == T.end())
+                B.M[B.card++] = this->M[i];
+        
+        return B;
+    }
+    
+    void operator=(const Multime &A) {
+        delete[] M;
+        card = A.card;
+        
+        M = new int [card];
+        for(int i=0;i < card; ++i)
+            M[i] = A.M[i];
+    }
+    
+    bool operator==(const Multime &A) {
+        if(card != A.card) return false;
+        
+        for(int i=0; i < card;++i)
+            if(M[i] != A.M[i]) return false;
+        
+        return true;
+    }
+    
+    bool operator!=(const Multime &A) {
+        if(card != A.card) return true;
+        
+        for(int i=0;i < card;++i)
+            if(A.M[i] != M[i]) return true;
+        
+        return false;
+    }
+    
+    bool operator<(const Multime &A) {
+        if(card < A.card) return false;
+        else return true;
+    }
+    
+    int get_card()
+    {
+        return card;
+    }
+    
+    int get_M_elem(int i)
+    {
+        if(card > i)
+            return M[i];
+        else
+            return -1;
+    }
+    
+private:
+    int *M;
+    int card;
 };
 
 class Pereche{
-
-	public:
-		Pereche(){
-			first = 0;
-			second = 0;
-		}
-
-		Pereche(int x, int y){
-			first = x;
-			second = y;
-		}
-
-		friend istream &operator>> (istream &in,Pereche &P)
-        {
-        	in>>P.first>>P.second;
-
-        	return in;
-        }
-
-        friend ostream &operator<< (ostream &out,Pereche &P){
-        	out<<P.first<<" "<<P.second<<"\n";
-
-        	return out;
-        }
-
-	private:
-		int first;
-		int second;
+    
+public:
+    Pereche(){
+        first = 0;
+        second = 0;
+    }
+    
+    Pereche(int x, int y){
+        first = x;
+        second = y;
+    }
+    
+    Pereche(const Pereche &P) {
+        first = P.first;
+        second = P.second;
+    }
+    
+    ~Pereche() {
+        
+    }
+    
+    friend istream &operator>> (istream &in,Pereche &P)
+    {
+        int x,y;
+        in>>x>>y;
+        
+        P.set_first(x); P.set_second(y);
+        return in;
+    }
+    
+    friend ostream &operator<< (ostream &out,Pereche &P){
+        out<<"("<<P.get_first()<<","<<P.get_second()<<")\n";
+        
+        return out;
+    }
+    
+    void operator=(const Pereche &P)
+    {
+        first = P.first;
+        second = P.second;
+    }
+    
+    bool operator==(const Pereche &P) {
+        if(first != P.first) return false;
+        if(second != P.second) return false;
+        
+        return true;
+    }
+    
+    bool operator!=(const Pereche &P) {
+        if((*this) == P) return false;
+        
+        return true;
+    }
+    
+    bool operator<(const Pereche &P) {
+        if(first < P.first) return true;
+        else if(first == P.first && second < P.second) return true;
+        else return false;
+    }
+    
+    void set_first(int x)
+    {
+        first = x;
+    }
+    
+    void set_second(int x)
+    {
+        second = x;
+    }
+    
+    int get_first()
+    {
+        return first;
+    }
+    
+    int get_second()
+    {
+        return second;
+    }
+    
+private:
+    int first;
+    int second;
 };
 
 class Multime_Pereche{
-	public:
-		Multime_Pereche(){
-			card = 0;
-		}
-
-		void insert_per(Pereche p)
-		{
-			int i;
-			for(i = 0;i < card;++i)
-				if(Mul_Per[i].first == p.first &&
-				   Mul_Per[i].second == p.second) return;
-
-			Mul_Per = (Pereche*)realloc(sizeof(Pereche) * (++card));
-			Mul_Per[card-1] = p;
-		}
-
-		friend istream &operator>> (istream &in, Multime_Pereche &A)
-		{
-			in>>A.card;
-            A.Mul_Per = (Pereche*)malloc(sizeof(Pereche) * A.card);
-            
-            for(int i = 0;i < A.card; ++i)
-                in>>A.Mul_Per[i];
-            
-        	///TO DO vec_mult function
-            
-            return in;
-		}
-
-		friend ostream &operator<< (ostream out, Multime_Pereche &A)
-		{
-			out<<A.card<<"\n";
-            
-            for(int i = 0;i < A.card; ++i)
-                out<<A.Mul_Per[i].first<<" "<<A.Mul_Per[i].second<<"\n";
-            
-        	///TO DO vec_mult function
-            
-            return out;
-		}
-
-	private:
-		Pereche *Mul_Per;
-		int card;
+    friend class Pereche;
+    
+public:
+    
+    /*void insert_per(Pereche p)
+    {
+        int i;
+        for(i = 0;i < card;++i)
+            if(Mul_Per[i].get_first() == p.get_first() &&
+               Mul_Per[i].get_second() == p.get_second()) return;
+        
+        Mul_Per.push_back(p);
+    }*/
+    
+    Multime_Pereche ()
+    {
+        
+    }
+    
+    Multime_Pereche (int n)
+    {
+        Mul_Per = new Pereche [n];
+        card = n;
+    }
+    
+    Multime_Pereche(const Multime_Pereche &A) {
+        card = A.card;
+        Mul_Per = new Pereche [card];
+        
+        for(int i = 0;i < card; ++i) {
+            Mul_Per[i] = A.Mul_Per[i];
+        }
+    }
+    
+    ~Multime_Pereche() {
+        delete [] Mul_Per;
+    }
+    
+    friend istream &operator>> (istream &in, Multime_Pereche &A)
+    {
+        Pereche x;
+        int n;
+        in>>n;
+        //A.Mul_Per = (Pereche*)malloc(sizeof(Pereche) * A.card);
+        A.Mul_Per = new Pereche [A.card];
+        
+        for(int i = 0;i < n; ++i)
+        {
+            in>>x;
+            A.Mul_Per[i] = x;
+        }
+        
+        ///TO DO vec_mult function
+        
+        return in;
+    }
+    
+    friend ostream &operator<< (ostream &out, Multime_Pereche &A)
+    {
+        out<<A.card<<"\n";
+        
+        for(int i = 0;i < A.card; ++i)
+            out<<"("<<A.Mul_Per[i].get_first()<<","<<A.Mul_Per[i].get_second()<<")\n";
+        
+        ///TO DO vec_mult function
+        
+        return out;
+    }
+    
+    void operator= (const Multime_Pereche &A) {
+        delete[] Mul_Per;
+        
+        card = A.card;
+        Mul_Per = new Pereche [card];
+        
+        for(int i = 0;i < card; ++i)
+            Mul_Per[i] = A.Mul_Per[i];
+        
+    }
+    
+    bool operator==(const Multime_Pereche &A) {
+        if(card != A.card) return false;
+        
+        for(int i=0; i < card;++i)
+            if(Mul_Per[i] != A.Mul_Per[i]) return false;
+        
+        return true;
+    }
+    
+    bool operator!=(const Multime_Pereche &A) {
+        if((*this) == A) return false;
+        
+        return true;
+    }
+    
+    void set_per(int i,Pereche x)
+    {
+        Mul_Per[i] = x;
+    }
+    
+private:
+    int card;
+    Pereche *Mul_Per;
 };
 
 Multime_Pereche prod_cartezian(Multime A, Multime B)
 {
-	Multime_Pereche C;
-
-	for(int i=0;i < A.card;++i)
-		for(int j=0;j < B.card;++j)
-		{
-			Pereche p(A.M[i], B.M[j]);
-			C.insert_per(p);
-		}
-
-	return C;
+    int ct = 0;
+    Multime_Pereche C(A.get_card() * B.get_card());
+    
+    for(int i=0;i < A.get_card();++i)
+        for(int j=0;j < B.get_card();++j)
+        {
+            Pereche p(A.get_M_elem(i), B.get_M_elem(j));
+            C.set_per(ct++, p);//Mul_Per[ct++] = p;//insert_per(p);
+        }
+    
+    return C;
 }
 
 int main()
 {
     Multime A,B;
     cin>>A>>B;
-    Multime C = A * B;
-    cout<<C;
+    
+    Multime C = A + B;
+    cout<<"AUB:\n"<<C;
+    
+    C = A * B;
+    cout<<"A intersectat cu B:\n"<<C;
+    
+    cout << (A != B) << "\n" << (A == C) << "\n";
+    
+    C = A - B;
+    cout<<"A - B:\n"<<C;
+    
+    C = B - A;
+    cout<<"B - A:\n"<<C;
+    
+    Multime_Pereche mp = prod_cartezian(A,B);
+    
+    cout<<"AxB:\n"<<mp;
+    
+    return 0;
 }
+
+
+
+
